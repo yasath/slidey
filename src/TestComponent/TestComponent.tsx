7aa91a6f249524ccd6b4c91af6538236c816d306
yasath <rayasathdias@gmail.com>
2022-11-21 23:41:53 +0000

    feat: add styled-components

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -1,18 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 import { TestComponentProps } from "./TestComponent.types";
 
-import "./TestComponent.css";
+const StyledDiv = styled.div`
+  background-color: grey;
+  border: 1px solid black;
+  padding: 16px;
+  width: 360px;
+  text-align: center;
+`;
+
+const StyledHeading = styled.h1`
+  font-size: 32px;
+`;
 
 const TestComponent: React.FC<TestComponentProps> = ({ heading, content }) => (
-    <div data-testid="test-component" className="test-component">
-        <h1 data-testid="test-component__heading" className="heading">
-            {heading}
-        </h1>
+    <StyledDiv data-testid="test-component">
+        <StyledHeading data-testid="test-component__heading">{heading}</StyledHeading>
         <div data-testid="test-component__content">
             {content}
         </div>
-    </div>
+    </StyledDiv>
 );
 
 export default TestComponent;
