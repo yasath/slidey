a658d8d2d099dbed6ed7ed0e628dc67b6283415f
yasath <rayasathdias@gmail.com>
2023-03-19 16:16:28 +0000

    feat: add custom favicon

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,21 @@
 import { addons } from '@storybook/addons';
 import theme from './theme';
 
+import faviconPNG from './favicon.png';
+import faviconSVG from './favicon.svg';
+
 addons.setConfig({
     theme: theme,
 });
+
+const link = document.createElement('link');
+link.setAttribute('rel', 'icon');
+link.setAttribute('type', 'image/svg+xml');
+link.setAttribute('href', faviconSVG);
+document.head.appendChild(link);
+
+const linkTwo = document.createElement('link');
+link.setAttribute('rel', 'icon');
+link.setAttribute('type', 'image/png');
+link.setAttribute('href', faviconPNG);
+document.head.appendChild(linkTwo);
